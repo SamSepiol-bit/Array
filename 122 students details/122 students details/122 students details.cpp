14e5d56c6f76acfc714d8bbe2902540d903da9eb
SamSepiol-bit <icbtworkfor@gmail.com>
2024-07-01 15:39:58 +0530

    Add files via upload

---
+++ 122 students details/122 students details/122 students details.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //array
+    string students[8] = { "Romesh", "Anjali", "pramod", "Athma", "Lesham", "Shamal","Shenutha", "Binara" };
+
+    cout << students[0] << endl;
+    cout << students[1] << endl;
+    cout << students[2] << endl;
+    cout << students[3] << endl;
+    cout << students[4] << endl;
+    cout << students[5] << endl;
+    cout << students[6] << endl;
+    cout << students[7] << endl;
+
+    cout << "size of student is: "<<sizeof(students);
+    
+
+}
+
